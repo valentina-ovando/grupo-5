7353a4418ba54b870c35915f98fdc326cbdcf773
valentina-ovando <valentinaovando702@gmail.com>
2025-09-15 17:04:59 -0300

    cambio1js

--- main.js
+++ main.js
@@ -2,8 +2,26 @@
 //Carrusel-Ramiro
 //Tarjetas-Conny
 //Footer-Valentina
-let footer = document.querySelector("#footer-valen")
+let footer = document.createElement("div")
 const lista1 = document.createElement("ul")
-let 
+let li = document.createElement("li")
+
+let li1 = document.createElement("li")
+li1.textContent = "holi1"
+
+let li2 = document.createElement("li")
+li2.textContent = "holi2"
+
+let li3 = document.createElement("li")
+li3.textContent = "holi3"
+
+let li4 = document.createElement("li")
+li4.textContent = "holi4"
+
+let li5 = document.createElement("li")
+li5.textContent = "holi5"
 
 
+
+
+ul.append(li, li1,li2,li3,li4,li5)
