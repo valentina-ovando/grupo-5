4a66ceb2da13b372b4e30f5db5b63ed5f098c789
valentina-ovando <valentinaovando702@gmail.com>
2025-09-15 14:30:27 -0300

    subo index

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 //Tarjetas-Conny
 //Footer-Valentina
 let footer = document.querySelector("#footer-valen")
-const footer1 = document.createElement("footer1")
+const lista1 = document.createElement("ul")
+let 
 
 
