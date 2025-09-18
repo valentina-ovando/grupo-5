d5697cc12effc876d56d93ff173ebe8502cc54e0
valentina-ovando <valentinaovando702@gmail.com>
2025-09-18 16:39:20 -0300

    cambiosfooter

--- main.js
+++ main.js
@@ -2,10 +2,19 @@
 //Carrusel-Ramiro
 //Tarjetas-Conny
 //Footer-Valentina
-let footer = document.createElement("div")
-const lista1 = document.createElement("ul")
-let li = document.createElement("li")
 
+//CREACION FOOTER
+let footer = document.querySelector("#footer")
+//Creacion LISTA 1
+let ul = document.createElement("ul")
+let lista1 = document.createElement("li")
+
+// Creacion LISTA 2
+let ul2 = document.createElement("ul")
+let lista2= document.createElement("li")
+
+
+//LISTADO1
 let li1 = document.createElement("li")
 li1.textContent = "holi1"
 
@@ -22,6 +31,48 @@ let li5 = document.createElement("li")
 li5.textContent = "holi5"
 
 
+//LISTADO2
+let liA = document.createElement("li")
+liA.textContent = "MJ"
+
+let liB = document.createElement("li")
+liB.textContent = "JM"
+
+let liC = document.createElement("li")
+liC.textContent = "HL"
+
+let liD = document.createElement("li")
+liD.textContent = "LH"
+
+//Creacion LISTA3
+let ul3 = document.createElement("ul")
+let lista3= document.createElement("li")
 
 
-ul.append(li, li1,li2,li3,li4,li5)
+//LISTADO3
+let liE = document.createElement("li")
+li1.textContent = "KHLG"
+
+let liF = document.createElement("li")
+li2.textContent = "JFKD"
+
+let liG = document.createElement("li")
+li3.textContent = "JKGF"
+
+let liH = document.createElement("li")
+li4.textContent = "JFKDL"
+
+let liJ = document.createElement("li")
+li5.textContent = "JGKILXD"
+
+
+
+
+ul.append(lista1, li1, li2, li3, li4, li5);
+footer.appendChild(ul)
+
+ul2.append(lista2,liA,liB,liC,liD)
+footer.appendChild(ul2)
+
+ul3.append(lista3,liE,liF,liG,liH,liJ)
+footer.appendChild(ul3)
